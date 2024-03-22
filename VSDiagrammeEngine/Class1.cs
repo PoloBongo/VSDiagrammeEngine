fe7c5b9a99400e7a485acbdc788fae8126ed2a7a
quentin lf <quentinleforestier0@gmail.com>
2024-03-22 12:29:28 +0100

    diagrame

---
+++ VSDiagrammeEngine/Class1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VSDiagrammeEngine
+{
+    public class Initializatio
+    {
+        public void Awak()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnEnable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Create()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class SceneRendering
+    {
+        public void PreLoad()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Render()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Visible()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Image()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Decommisoing
+    {
+        public void OnDisable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Quit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Destroy()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Pause()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class Physics
+    {
+        public void OnCollision()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnTrigger()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class InputEnvent
+    {
+        public void InputPress()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void InputDown()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class GameLogic
+    {
+
+        public void Update()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
